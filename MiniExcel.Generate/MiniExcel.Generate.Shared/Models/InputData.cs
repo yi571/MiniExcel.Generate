63be345a63440fb82115a294ab3cab61b000df3e
yi571 <yi571@outlook.com>
2021-04-21 10:36:30 +0800

    加入專案檔案。

---
+++ MiniExcel.Generate/MiniExcel.Generate.Shared/Models/InputData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniExcel.Generate.Shared.Models
+{
+    public class InputData
+    {
+        [Display(Name = "連線字串")]
+        [Required(ErrorMessage = "{0}為必填")]
+        public string ConnectStr { get; set; }
+
+        [Display(Name = "檔案名稱")]
+        [Required(ErrorMessage = "{0}為必填")]
+        public string FileName { get; set; }
+
+        [Display(Name = "查詢指令")]
+        [Required(ErrorMessage = "{0}為必填")]
+        public string SqlCommand { get; set; }
+        [Display(Name = "檔案類型")]
+        public FileType FileType { get; set; }
+    }
+
+    public enum FileType
+    {
+        Excel,
+        Csv
+    }
+}
---
+++ MiniExcel.Generate/MiniExcel.Generate.Shared/Pages/Index.razor.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Components.Forms;
+using MiniExcel.Generate.Shared.Models;
+using MiniExcelLibs;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniExcel.Generate.Shared.Pages
+{
+    public sealed partial class Index
+    {
+        private InputData Model { get; set; } = new() {
+            ConnectStr = @"Data Source=.;Initial Catalog=YTest;Connection Timeout=300000;Integrated Security=true;"
+            , SqlCommand = "select * from Person"
+            , FileType = FileType.Excel };
+
+
+        private async Task OnValidSubmit(EditContext context)
+        {
+            using (var connection = new SqlConnection(Model.ConnectStr))
+            {
+                var rows = connection.Query(Model.SqlCommand);
+                //iniExcelLibs.MiniExcel.SaveAs(path, rows);
+                string fuDangMing = "";
+                switch (Model.FileType)
+                {
+                    case FileType.Excel:
+                        fuDangMing = ".xlsx";
+                        break;
+                    case FileType.Csv:
+                        fuDangMing = ".csv";
+                        break;
+                    default:
+                        break;
+                }
+                string path = $@"D:\Tmp\{Model.FileName}{fuDangMing}";
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                MiniExcelLibs.MiniExcel.SaveAs(path, rows);
+            }
+        }
+
+        private Task OnInvalidSubmit(EditContext context)
+        {
+            //Trace2.Log("OnInvalidSubmit 回调委托");
+            return Task.CompletedTask;
+        }
+    }
+
+}
